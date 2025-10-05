a85b29923c95bfd6a51f9b3d0d11a37484314204
farhansyedali <syedfarhanali2005@gmail.com>
2025-10-05 14:34:15 +0500

    feature 1 done

---
+++ Makefile
@@ -0,0 +1,15 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -std=c99
+SRC = src/ls-v1.0.0.c
+BIN = bin/ls-v1.0.0
+
+.PHONY: all clean
+
+all: $(BIN)
+
+$(BIN): $(SRC)
+	mkdir -p bin
+	$(CC) $(CFLAGS) $(SRC) -o $(BIN)
+
+clean:
+	rm -rf bin
---
+++ src/ls-v1.0.0.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+extern int errno;
+
+void do_ls(const char *dir);
+
+int main(int argc, char const *argv[])
+{
+    if (argc == 1)
+    {
+        do_ls(".");
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            printf("Directory listing of %s : \n", argv[i]);
+            do_ls(argv[i]);
+	    puts("");
+        }
+    }
+    return 0;
+}
+
+void do_ls(const char *dir)
+{
+    struct dirent *entry;
+    DIR *dp = opendir(dir);
+    if (dp == NULL)
+    {
+        fprintf(stderr, "Cannot open directory : %s\n", dir);
+        return;
+    }
+    errno = 0;
+    while ((entry = readdir(dp)) != NULL)
+    {
+        if (entry->d_name[0] == '.')
+            continue;
+        printf("%s\n", entry->d_name);
+    }
+
+    if (errno != 0)
+    {
+        perror("readdir failed");
+    }
+
+    closedir(dp);
+}
